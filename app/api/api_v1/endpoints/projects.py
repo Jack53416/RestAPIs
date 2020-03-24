21a42607b9a850c6e6b144f04303bd82c4acc8af
Jacek Byczkiewicz <jacek.byczkiewicz@trw.com>
2020-03-24 15:07:15 +0100

    Create patch endpoint for project user

--- app/api/api_v1/endpoints/projects.py
+++ app/api/api_v1/endpoints/projects.py
@@ -29,3 +29,18 @@ def create_project_user(project_user: schemas.ProjectUserCreate, db: Session = D
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User is already in this project')
     project_user = crud.project_user.create(db, obj_in=project_user)
     return project_user
+
+
+@router.patch('/user/{association_id}', response_model=schemas.ProjectUser)
+def update_project_user_role(*,
+                             db: Session = Depends(get_db),
+                             association_id: int,
+                             project_user_in: schemas.ProjectUserUpdate):
+
+    project_user = crud.project_user.get(db, association_id)
+    if not project_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Searched object does not exist in database')
+    project_user = crud.project_user.update(db, db_obj=project_user, obj_in=project_user_in)
+    return project_user
+
