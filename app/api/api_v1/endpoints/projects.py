3dc1ac672b202b837701968f356f788433d03e2d
Jacek Byczkiewicz <jacek.byczkiewicz@trw.com>
2020-03-24 15:16:48 +0100

    Secure existing endpoints

--- app/api/api_v1/endpoints/projects.py
+++ app/api/api_v1/endpoints/projects.py
@@ -7,24 +7,34 @@ from starlette import status
 from app import crud
 from app import schemas
 from app.api.utils.db import get_db
+from app.api.utils.security import get_current_active_user
+from app.models.user import User as DBUser
 
 router = APIRouter()
 
 
 @router.get("/", response_model=List[schemas.Project])
-def read_projects(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_projects(skip: int = 0,
+                  limit: int = 100,
+                  db: Session = Depends(get_db),
+                  current_user: DBUser = Depends(get_current_active_user)):
     projects = crud.project.get_multi(db, skip=skip, limit=limit)
     return projects
 
 
 @router.get('/user', response_model=List[schemas.ProjectUser])
-def read_project_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_project_users(skip: int = 0,
+                       limit: int = 100,
+                       db: Session = Depends(get_db),
+                       current_user: DBUser = Depends(get_current_active_user)):
     associations = crud.project_user.get_multi(db, skip=skip, limit=limit)
     return associations
 
 
 @router.post('/user', response_model=schemas.ProjectUser)
-def create_project_user(project_user: schemas.ProjectUserCreate, db: Session = Depends(get_db)):
+def create_project_user(project_user: schemas.ProjectUserCreate,
+                        db: Session = Depends(get_db),
+                        current_user: DBUser = Depends(get_current_active_user)):
     if crud.project_user.exists(db, user_id=project_user.user_id, project_id=project_user.project_id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User is already in this project')
     project_user = crud.project_user.create(db, obj_in=project_user)
@@ -35,7 +45,8 @@ def create_project_user(project_user: schemas.ProjectUserCreate, db: Session = D
 def update_project_user_role(*,
                              db: Session = Depends(get_db),
                              association_id: int,
-                             project_user_in: schemas.ProjectUserUpdate):
+                             project_user_in: schemas.ProjectUserUpdate,
+                             current_user: DBUser = Depends(get_current_active_user)):
 
     project_user = crud.project_user.get(db, association_id)
     if not project_user:
--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -7,12 +7,16 @@ from starlette import status
 from app import crud
 from app import schemas
 from app.api.utils.db import get_db
+from app.api.utils.security import get_current_active_superuser, get_current_active_user
+from app.models.user import User as DBUser
 
 router = APIRouter()
 
 
 @router.post("/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate,
+                db: Session = Depends(get_db),
+                current_user: DBUser = Depends(get_current_active_superuser)):
     db_user = crud.user.get_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -20,13 +24,18 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0,
+               limit: int = 100,
+               db: Session = Depends(get_db),
+               current_user: DBUser = Depends(get_current_active_user)):
     users = crud.user.get_multi(db, skip=skip, limit=limit)
     return users
 
 
 @router.get("/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int,
+              db: Session = Depends(get_db),
+              current_user: DBUser = Depends(get_current_active_user)):
     db_user = crud.user.get(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
