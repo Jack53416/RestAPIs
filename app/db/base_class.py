2c394d545d7a65040e3e5d32f73da510936c3e2c
Jacek Byczkiewicz <jacek.byczkiewicz@trw.com>
2020-03-25 16:51:14 +0100

    Implement paginator class

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,3 +1,21 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import inspect
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
 
-Base = declarative_base()
+
+def create_col_lookup(mapper):
+    return {c.name: k for k, c in mapper.columns.items()}
+
+
+class ColBase(object):
+
+    @classmethod
+    def create_col_lookup(cls):
+        return {c.name: k for k, c in inspect(cls).mapper.columns.items()}
+
+    @classmethod
+    def map_db_columns(cls, **kwargs):
+        mapping = cls.create_col_lookup()
+        return {mapping[k]: v for k, v in kwargs.items() if k in mapping}
+
+
+Base = declarative_base(cls=ColBase)
